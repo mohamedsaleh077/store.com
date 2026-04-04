794ff33497860c26be0467a1d2310200b971a2b5
Mohammed Saleh <mohammed.saleh.22707@gmail.com>
2026-04-05 01:20:53 +0200

    init the app

--- websites/app/init.php
+++ websites/app/init.php
@@ -4,3 +4,18 @@ require_once "../app/vendor/autoload.php";
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+
+use Core\App;
+use Core\Router;
+use Core\View;
+use Core\Middleware;
+use Core\SessionManager;
+use Core\Container;
+
+$container = new Container();
+$container->Add(SessionManager::class, fn($c) => new SessionManager);
+$container->Add(Middleware::class, fn($c) => new Middleware($c->Make(SessionManager::class)));
+$container->Add(Router::class, fn($c) => new Router(new View(), $c->Make(Middleware::class)));
+$container->Add(App::class, fn($c) => new App($c->Make(Router::class)));
+
+$app = $container->Make(App::class);
