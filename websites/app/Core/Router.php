616281c2b2e3fe5d241e7dc70efa5639fe598cbc
Mohammed Saleh <mohammed.saleh.22707@gmail.com>
2026-04-05 00:12:42 +0200

    Initlizing the Router and setting up Add and GoTo Methods

--- websites/app/Core/Router.php
+++ websites/app/Core/Router.php
@@ -2,17 +2,63 @@
 declare(strict_types=1);
 
 namespace Core;
-use Core\View;
-use Traits\Errors;
+use Interfaces\iView;
 
+use function count;
+use function http_response_code;
 
 /**
  * Description of Router
  * Dummy Router
+ * [
+ *      GET => [
+ *                  "/test" => [
+ *                                  "target" => "namespace\Controller@Method",
+ *                                  "rules" => ["rule1", "rule2", "rule3"], 
+ *                                  "neededParams" => 4
+ *                              ],
+ *                  "home" => ["#landing/index", []]
+ *          ]
+ * ]
  * @author mohamed
  */
 class Router {
-    public function __construct() {
-    }   
+    private static array $routes = []; 
     
+    public function __construct(private iView $Viewer) {  
+    }
+    
+    public function Add(string $method, string $route, string $target, array $rules = [], int $allowedParams = 0): void
+    {
+        self::$routes[$method][ $route] = ["target" => $target ,"rules" => $rules, "neededParams" => $allowedParams];
+    }
+    
+    // URI: /unit/subunit/param1/param2 and who hand this is the App class.
+    public function GoTo(string $method, string $route, array $params): void
+    {
+        if(!isset(self::$routes[$method])){
+            http_response_code(405);
+            die();
+        }
+        
+        if(!isset(self::$routes[$method][$route])){
+            http_response_code(404);
+            die();
+        }
+        
+        $route = self::$routes[$method][$route];
+        
+        if(count($params) !== $route["neededParams"]){
+            http_response_code(400);
+            die();
+        }
+                
+        if(str_starts_with($route["target"], "#")){
+           $unhashed = substr($route["target"], 1);
+           $this->Viewer::HTML($unhashed);
+        }else{
+            $targetArr = explode("@", $route["target"]);
+            call_user_func_array([(new $targetArr[0]), $targetArr[1]], $params);
+        }
+    }
 }
