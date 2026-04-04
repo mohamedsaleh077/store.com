dd2b5f5a092cc42e762ee686c6bfc3c41ac9bab4
Mohammed Saleh <mohammed.saleh.22707@gmail.com>
2026-04-05 00:11:57 +0200

    setting up interface for Viewers and setup the View class with this Interface

--- websites/app/Core/View.php
+++ websites/app/Core/View.php
@@ -2,18 +2,17 @@
 declare(strict_types=1);
 
 namespace Core;
+use Interfaces\iView;
 
 /**
  * Description of View
  *
  * @author mohamed
  */
-class View {
+class View
+implements iView
+{
     public static function HTML($page, $data = []){
         require_once $_SERVER["DOCUMENT_ROOT"] . "/app/View/" . $page . ".php";
-    }
-    
-    public static function RoutesView($page_title, $routes){
-        require_once $_SERVER["DOCUMENT_ROOT"] . "/app/View/templates/routes.php";
     }
 }
